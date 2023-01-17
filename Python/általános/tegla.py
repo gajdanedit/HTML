9de13b928d77eb27866c790d552e2e4925fca38b
gajdanedit <gajdanedit1129@gmail.com>
2023-01-17 14:07:41 +0100

    main

--- Python/általános/tegla.py
+++ Python/általános/tegla.py
@@ -1,6 +1,6 @@
-from classes import *
+from classes import*
 
-t1 = Téglalap()
+t1 = Teglalap()
 t1.SetA(int(input("Írd be a téglalap \"a\" oldalát: ")))
 t1.SetB(int(input("Írd be a téglalap \"b\" oldalát: ")))
 print("A t1 kerülete: {0}. Területe: {1}".format(t1.GetKerület(), t1.GetTerület()))
